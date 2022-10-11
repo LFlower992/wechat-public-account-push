3964618a1d357c94d722c8e8cddfb173a9547579
LFlower992 <480411991@qq.com>
2022-10-11 19:26:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,11 +26,11 @@ USE_PASSAGE: 'server-chan',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-04',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2000', date: '07-04',
+          type: '生日', name: '宝贝', year: '2000', date: '07-04',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -39,9 +39,8 @@ USE_PASSAGE: 'server-chan',
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-01-01' },
+        
       ],
     },
   ],
