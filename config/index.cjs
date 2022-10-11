d5225de8f1831d2cfc643214ffccfc27c107e74f
LFlower992 <480411991@qq.com>
2022-10-11 18:31:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,10 +28,6 @@ USE_PASSAGE: 'server-chan',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '2002', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '2000', date: '07-04',
@@ -52,13 +48,13 @@ USE_PASSAGE: 'server-chan',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'osPYT57L4a4qfue17FVv96Zlswaw',
     }
   ],
 
