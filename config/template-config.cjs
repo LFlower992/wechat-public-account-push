c3fe9f583704b3c9e32059ef14fdb715fc2fda68
LFlower992 <480411991@qq.com>
2022-10-11 19:45:36 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,19 +2,19 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '亲爱的咕咕宝贝, 早上好呀~',
     desc: `
       **{{date.DATA}}**
       下个休息日：{{holidaytts.DATA}}
       ---
       城市：{{city.DATA}}
-      天气：{{weather.DATA}}
+      今天的天气是：{{weather.DATA}}
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       风向: {{wind_direction.DATA}}
       风级: {{wind_scale.DATA}}
       {{comprehensive_horoscope.DATA}}
       ---
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们相识的第{{love_day.DATA}}天哟
       {{birthday_message.DATA}}
       ---
       {{moment_copyrighting.DATA}}
