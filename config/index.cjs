89e83698596d3dcbcd445f82cc9a2f34789f43a6
LFlower992 <480411991@qq.com>
2022-10-11 19:49:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,9 +20,9 @@ USE_PASSAGE: 'server-chan',
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '咕咕宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT176007TLNkGFLZo4OZ2ONEFO3ZtUVaK',
+      id: 'SCT176029TQebyT8WPdWKvDbTWRcgIMKbB',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -30,10 +30,10 @@ USE_PASSAGE: 'server-chan',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝', year: '2000', date: '07-04',
+          type: '生日', name: '我们的咕咕宝贝', year: '2000', date: '07-04',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2021', date: '10-30',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
